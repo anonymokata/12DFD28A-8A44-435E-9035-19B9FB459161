3424f81399b66b7aa164b6fefb94ba17a83ae7ea
anonymous <anonymous>
2016-07-13 20:36:01 -0400

    clean up work

--- anon.sh
+++ anon.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
 
 # determine name of organization 
-echo -n "Enter the name of the organization. (enter Q to quit): "
+echo -n "Enter the name of the organization. [anonymokata] : "
 read -a ORGANIZATION
-remove=${ORGANIZATION:-anonymokata}
+ORGANIZATION=${ORGANIZATION:-anonymokata}
 echo ""
-
-if [ ${ORGANIZATION[0]} = "Q" ] || [ ${ORGANIZATION[0]} = "q" ]; then
-  exit 0
-fi
 
 # determine name of user 
-echo -n "Enter the name of the user within $ORGANIZATION. (enter Q to quit): "
+echo -n "Enter the name of the user within $ORGANIZATION. [ddaugher] : "
 read -a USER
-remove=${USER:-Q}
+USER=${USER:-ddaugher}
 echo ""
-
-if [ ${USER[0]} = "Q" ] || [ ${USER[0]} = "q" ]; then
-  exit 0
-fi
 
 # determine name of new repository
-echo -n "Enter the name of the new repository. (enter Q to quit): "
+UUID=$(uuidgen)
+echo -n "Enter the name of the new repository. ["$UUID"] : "
 read -a name
-remove=${name:-Q}
+name=${name:-$UUID}
 echo ""
 
-if [ ${name[0]} = "Q" ] || [ ${name[0]} = "q" ]; then
-  exit 0
-fi
-
 # clone repository
-echo -n "Enter the repository URL to clone. (enter Q to quit): "
+echo -n "Enter the candidate repository URL to clone : "
 read -a repository
-remove=${repository:-Q}
+repository=${repository:-Q}
 echo ""
 
 if [ ${repository[0]} = "Q" ] || [ ${repository[0]} = "q" ]; then
