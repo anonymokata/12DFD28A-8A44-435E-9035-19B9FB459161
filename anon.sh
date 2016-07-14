6cfdeb3b80fc4117f349defaef860d4f944568c8
anonymous <anonymous>
2016-07-13 20:05:24 -0400

    working on script

---
+++ anon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USER=ddaugher
+ORGANIZATION=anonymokata
+
+curl -u "$USER" https://api.github.com/orgs/$ORGANIZATION/repos -d '{"'"name"'":"'"$1"'"}'
+git remote add origin https://github.com:/$ORGANIZATION/$1.git
+git push -u origin master
+
+#git filter-branch --env-filter '
+#export GIT_AUTHOR_EMAIL="anonymous"
+#export GIT_AUTHOR_NAME="anonymous"
+#' -- --all
+
